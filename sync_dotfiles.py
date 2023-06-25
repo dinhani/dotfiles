c7f8fa9ff5bf2568bddc23598ca799a815346023
Renato Dinhani <renatodinhani@gmail.com>
2023-06-25 00:54:44 -0300

    refactor sync script

--- sync_dotfiles.py
+++ sync_dotfiles.py
@@ -1,6 +1,10 @@
 import os
 import shutil
 
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+
 def user():
     """User to connect to remote host."""
     return os.environ["REMOTE_USER"]
@@ -9,30 +13,38 @@ def host():
     """Remote host to connect to."""
     return os.environ["REMOTE_HOST"]
 
-def appdata():
-    """Windows appdata folder."""
-    return os.environ["APPDATA"]
+def appdata(path):
+    """Path inside Windows appdata folder."""
+    return os.environ["APPDATA"] + "/" + path
 
-def local_appdata():
-    """Windows local appdata folder."""
-    return os.environ["LOCALAPPDATA"]
+def local_appdata(path):
+    """Path inside Windows local appdata folder."""
+    return os.environ["LOCALAPPDATA"] + "/" + path
 
-def sync_remote(file):
-    """Syncs a remote file to the local backup."""
+def dotfiles(path = None):
+    """Path inside dotfiles folder."""
+    base = "./dotfiles"
+    if path is None:
+        return base
+    else:
+        return base + "/" + path
+
+def copy_remote_file(file, target):
+    """Copies a remote file to a local target."""
     print("Remote: {}".format(file))
 
     # create folder if necessary
-    file_dirname = "dotfiles/" + os.path.dirname(file)
-    file_basename = os.path.basename(file)
-    if file_dirname != "" and not os.path.exists(file_dirname):
-        os.makedirs(file_dirname)
+    target_dirname = os.path.dirname(target)
+    target_basename = os.path.basename(target)
+    if target_dirname != "" and not os.path.exists(target_dirname):
+        os.makedirs(target_dirname)
 
     # execute sync command
-    command = "scp {}@{}:~/{} {}/{} > out".format(user(), host(), file, file_dirname, file_basename)
+    command = "scp {}@{}:~/{} {}/{} > out".format(user(), host(), file, target_dirname, target_basename)
     os.system(command)
 
-def sync_local_folder(source, target):
-    """Syncs a local folder to the local backup."""
+def copy_local_folder(source, target):
+    """Copies a local folder to a local target."""
     print("Local: {} -> {}".format(source, target))
 
     # create folder if necessary
@@ -41,8 +53,8 @@ def sync_local_folder(source, target):
 
     shutil.copytree(source, target, dirs_exist_ok=True)
 
-def sync_local_file(source, target):
-    """Syncs a local file to the local backup."""
+def copy_local_file(source, target):
+    """Copies a local file to a local target."""
     print("Local: {} -> {}".format(source, target))
 
     # create folder if necessary
@@ -52,29 +64,34 @@ def sync_local_file(source, target):
 
     shutil.copyfile(source, target)
 
+# ------------------------------------------------------------------------------
+# Execution
+# ------------------------------------------------------------------------------
+
 if __name__ == "__main__":
     print("Remote: {}@{}".format(user(), host()))
 
     # remove synced files
-    shutil.rmtree("dotfiles")
+    if os.path.exists(dotfiles()):
+        shutil.rmtree(dotfiles())
 
     # sync remote to local
-    sync_remote("scripts/alias.sh")
-    sync_remote(".vimrc")
-    sync_remote(".config/helix/config.toml")
-    sync_remote(".config/helix/languages.toml")
+    copy_remote_file("scripts/alias.sh", dotfiles("scripts/alias.sh"))
+    copy_remote_file(".vimrc", dotfiles(".vimrc"))
+    copy_remote_file(".config/helix/config.toml", dotfiles(".config/helix/config.toml"))
+    copy_remote_file(".config/helix/languages.toml", dotfiles(".config/helix/languages.toml"))
 
     # sync local to local
-    sync_local_file(appdata() + "/Code/User/keybindings.json", "dotfiles/vscode/keybindings.json")
-    sync_local_file(appdata() + "/Code/User/settings.json", "dotfiles/vscode/settings.json")
-    sync_local_folder(appdata() + "/JetBrains/IdeaIC2023.1/keymaps", "dotfiles/intellij/keymaps")
-    sync_local_file(appdata() + "/JetBrains/IdeaIC2023.1/options/editor.xml", "dotfiles/intellij/options/editor.xml")
-    sync_local_file(appdata() + "/JetBrains/IdeaIC2023.1/options/editor-font.xml", "dotfiles/intellij/options/editor-font.xml")
-    sync_local_file(appdata() + "/JetBrains/IdeaIC2023.1/options/window.layouts.xml", "dotfiles/intellij/options/window.layouts.xml")
-    sync_local_file(local_appdata() + "/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json", "dotfiles/windows-terminal/settings.json")
+    copy_local_file(appdata("/Code/User/keybindings.json"), dotfiles("vscode/keybindings.json"))
+    copy_local_file(appdata("/Code/User/settings.json"), dotfiles("vscode/settings.json"))
+    copy_local_folder(appdata("/JetBrains/IdeaIC2023.1/keymaps"), dotfiles("intellij/keymaps"))
+    copy_local_file(appdata("/JetBrains/IdeaIC2023.1/options/editor.xml"), dotfiles("intellij/options/editor.xml"))
+    copy_local_file(appdata("/JetBrains/IdeaIC2023.1/options/editor-font.xml"), dotfiles("intellij/options/editor-font.xml"))
+    copy_local_file(appdata("/JetBrains/IdeaIC2023.1/options/window.layouts.xml"), dotfiles("intellij/options/window.layouts.xml"))
+    copy_local_file(local_appdata("/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"), dotfiles("windows-terminal/settings.json"))
 
     # distribute local to local
-    sync_local_folder("dotfiles/.config/helix", appdata() + "/helix")
+    copy_local_folder(dotfiles(".config/helix"), appdata("helix"))
 
     # remove temp files created
     os.remove("out")
