17ef3b7cafc27c1f882eb488c9e09fb13ef831ee
Renato Dinhani <renatodinhani@gmail.com>
2024-04-01 01:26:51 -0300

    feat: download and more refactoring

--- bin-server.rb
+++ bin-server.rb
@@ -1,41 +1,52 @@
-require 'fileutils'
-require 'json'
-require 'sinatra'
+require "fileutils"
+require "json"
+require "sinatra"
 
-# ------------------------------------------------------------------------------
-# Server setup
-# ------------------------------------------------------------------------------
-set :bind, '0.0.0.0'
+set :bind, "0.0.0.0"
 set :port, 3000
 
-# ------------------------------------------------------------------------------
-# Endpoint to validate everything is ok
-# ------------------------------------------------------------------------------
-get '/health' do
-    'ok'
+get "/health" do
+    "ok"
+end
+
+# Receive file path and content
+post "/upload" do
+    # parse input
+    payload = parse_payload(request)
+    path = parse_path(payload)
+
+    # create path dir if not exists
+    path_dir = File.dirname(path)
+    FileUtils.mkdir_p(path_dir) unless File.directory?(path_dir)
+
+    # write file content
+    puts "Writing: #{path}"
+    File.open(path, "w") do |f|
+        f.write(payload["content"])
+    end
+
+    "ok"
+end
+
+post "/download" do
+    # parse input
+    payload = parse_payload(request)
+    path = parse_path(payload)
+
+    # read file content
+    puts "Reading: #{path}"
+    File.open(path, "r").read
 end
 
 # ------------------------------------------------------------------------------
-# Receive file path and content
+# Helpers
 # ------------------------------------------------------------------------------
-post '/upload' do
-    # parse payload
-    payload = JSON.parse(request.body.read)
 
-    # extract path
-    path = payload['path']
-    path = File.expand_path(path)
+private def parse_payload(request)
+    JSON.parse(request.body.read)
+end
 
-    # create path if not exists
-    path_dir = File.dirname(path)
-    puts "Creating path if necessary: #{path_dir}"
-    FileUtils.mkdir_p(path_dir) unless File.directory?(path_dir)
-
-    # get file content and save it
-    puts "Writing path: #{path}"
-    File.open(path, "w") do |f|
-        f.write(payload['content'])
-    end
-
-    'ok'
+private def parse_path(payload)
+    path = payload["path"]
+    File.expand_path(path)
 end
