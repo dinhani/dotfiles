bd7496311494a0c19917ae4e447a15b8f7e41720
Renato Dinhani <renatodinhani@gmail.com>
2024-09-17 00:49:11 -0300

    refactor: remove emu subfolder

--- dotfiles.py
+++ dotfiles.py
@@ -158,17 +158,17 @@ def backup():
     # --------------------------------------------------------------------------
     if is_win():
         # Emulators
-        win_roaming("Dolphin Emulator/Config") >> dotfiles("emu/dolphin")
-        win_emu("cemu2/controllerProfiles") >> dotfiles("emu/cemu2/controllerProfiles")
-        win_emu("cemu2/settings.xml") >> dotfiles("emu/cemu2/settings.xml")
-        win_emu("mame/mame.ini") >> dotfiles("emu/mame/mame.ini")
-        win_emu("mame/plugin.ini") >> dotfiles("emu/mame/plugin.ini")
-        win_emu("mame/ui.ini") >> dotfiles("emu/mame/ui.ini")
-        win_emu("pcsx2/inis/PCSX2.ini") >> dotfiles("emu/pcsx2/inis/PCSX2.ini")
-        win_emu("retroarch/retroarch.cfg") >> dotfiles("emu/retroarch/retroarch.cfg")
-        win_emu("retroarch/retroarch-core-options.cfg") >> dotfiles("emu/retroarch/retroarch-core-options.cfg")
-        win_emu("retroarch/retroarch_qt.cfg") >> dotfiles("emu/retroarch/retroarch_qt.cfg")
-        win_roaming("Ryujinx/Config.json") >> dotfiles("emu/ryujinx/Config.json")
+        win_roaming("Dolphin Emulator/Config") >> dotfiles("dolphin")
+        win_emu("cemu2/controllerProfiles") >> dotfiles("cemu2/controllerProfiles")
+        win_emu("cemu2/settings.xml") >> dotfiles("cemu2/settings.xml")
+        win_emu("mame/mame.ini") >> dotfiles("mame/mame.ini")
+        win_emu("mame/plugin.ini") >> dotfiles("mame/plugin.ini")
+        win_emu("mame/ui.ini") >> dotfiles("mame/ui.ini")
+        win_emu("pcsx2/inis/PCSX2.ini") >> dotfiles("pcsx2/inis/PCSX2.ini")
+        win_emu("retroarch/retroarch.cfg") >> dotfiles("retroarch/retroarch.cfg")
+        win_emu("retroarch/retroarch-core-options.cfg") >> dotfiles("retroarch/retroarch-core-options.cfg")
+        win_emu("retroarch/retroarch_qt.cfg") >> dotfiles("retroarch/retroarch_qt.cfg")
+        win_roaming("Ryujinx/Config.json") >> dotfiles("ryujinx/Config.json")
 
         # IntelliJ
         win_roaming("JetBrains/IdeaIC2023.2/keymaps") >> dotfiles("intellij/keymaps")
@@ -209,12 +209,12 @@ def restore():
     # --------------------------------------------------------------------------
     if is_win():
         # Emulators
-        dotfiles("emu/cemu2") >> win_emu("cemu2")
-        dotfiles("emu/dolphin") >> win_roaming("Dolphin Emulator/Config")
-        dotfiles("emu/mame") >> win_emu("mame")
-        dotfiles("emu/pcsx2") >> win_emu("pcsx2")
-        dotfiles("emu/retroarch") >> win_emu("retroarch")
-        dotfiles("emu/ryujinx") >> win_roaming("Ryujinx")
+        dotfiles("cemu2") >> win_emu("cemu2")
+        dotfiles("dolphin") >> win_roaming("Dolphin Emulator/Config")
+        dotfiles("mame") >> win_emu("mame")
+        dotfiles("pcsx2") >> win_emu("pcsx2")
+        dotfiles("retroarch") >> win_emu("retroarch")
+        dotfiles("ryujinx") >> win_roaming("Ryujinx")
 
         # Helix
         dotfiles("helix") >> win_roaming("helix")
