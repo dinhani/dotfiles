81b23bc253d55103466efb76fb6f382087748ccc
Renato Dinhani <renatodinhani@gmail.com>
2024-09-18 04:11:00 -0300

    chore: update mac asdf tool versions and vscode settings

--- dotfiles.py
+++ dotfiles.py
@@ -110,7 +110,8 @@ def win_emu(path: str) -> File:
 
 def mac_app_support(path: str) -> File:
     """Path of Mac Application Support directory."""
-    return unix_home("Library/Application Support/") + path
+    base = unix_home("Library/Application Support/")
+    return File("f{base}/{path}")
 
 def dotfiles(path: str = None) -> File:
     """Path of dotfiles backup directory."""
