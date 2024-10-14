adfc718a30b5fb5c5a4009b4fc8724c06bdee050
Renato Dinhani <renatodinhani@gmail.com>
2024-10-14 20:37:44 -0300

    feat(powershell): expand profile script

--- dotfiles/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,19 @@
 # ------------------------------------------------------------------------------
-# Examples
-# ------------------------------------------------------------------------------
-# Install-Module -Name ThreadJob
-# $jobs = @()
-# ForEach-Object {
-#     $jobs += Start-ThreadJob -StreamingHost $Host -ScriptBlock  {
-#         $arg = $args[0]
-#     } -ThrottleLimit 10 -ArgumentList $_
-# }
-# Wait-Job -Job $jobs
-
-# ------------------------------------------------------------------------------
 # Functions
 # ------------------------------------------------------------------------------
+
+# Prints each argument passed to the function with its index.
+function Write-Args {
+    for($i = 0; $i -lt $args.length; $i++) {
+        "$i`: $($args[$i])"
+    }
+}
 
 # Archive each file in the given path to 7zip or zip.
 function Invoke-Archive {
     param (
         [Parameter(Mandatory = $true)]
-        [string[]]$InputPath,
+        [string]$InputPath,
 
         [Parameter(Mandatory = $false)]
         [string]$OutputPath,
@@ -30,6 +25,9 @@ function Invoke-Archive {
         [Parameter(Mandatory = $false)]
         [ValidateRange(0, 9)]
         [int]$Level = 9,
+
+        [Parameter(Mandatory = $false)]
+        [int]$Limit = 2,
 
         [Parameter(Mandatory = $false)]
         [switch]$Directory,
@@ -57,19 +55,22 @@ function Invoke-Archive {
     Write-Host "Found $($entries.Count) entries."
 
     # archive entries
-    foreach ($entry in $entries) {
-        # ignore already archived entries
-        if (Test-Path $entry -PathType Leaf) {
+    $entries | ForEach-Object -ThrottleLimit $Limit -Parallel {
+        # generate archive name
+        $archive = Join-Path $using:OutputPath "$($_.BaseName).$using:Type"
+        if (Test-Path -LiteralPath $archive -PathType Leaf) {
+            Write-Warning "Skipping: $archive"
             continue
         }
 
-        # generate archive name
-        $archive = Join-Path $OutputPath "$($entry.Name).$Type"
-        Write-Host "Archiving: $archive"
-
         # archive entry
-        $command = "7z a -t$Type -mmt16 -mx=$Level -bso0 -bsp0 $archive $($entry.FullName)\*"
-        Write-Host $command
+        Write-Host "Archiving: $archive"
+        if ($_.PSIsContainer) {
+            $command = "7z a -t$($using:Type) -mx=$($using:Level) -bso0 -bsp0 `"$archive`" `"$($_.FullName)\*`""
+        } else {
+            $command = "7z a -t$($using:Type) -mx=$($using:Level) -bso0 -bsp0 `"$archive`" `"$($_.FullName)`""
+        }
+        Write-Host "$command"
         Invoke-Expression $command
     }
 }
@@ -78,7 +79,7 @@ function Invoke-Archive {
 function Invoke-Unarchive {
     param (
         [Parameter(Mandatory = $true)]
-        [string[]]$InputPath,
+        [string]$InputPath,
 
         [Parameter(Mandatory = $false)]
         [string]$OutputPath
@@ -95,28 +96,92 @@ function Invoke-Unarchive {
     Write-Host "Found $($entries.Count) entries."
 
     # unarchive entries
-    foreach ($entry in $entries) {
-        # ignore already unarchived entries
-        if (Test-Path $entry -PathType Container) {
+    $entries | ForEach-Object -ThrottleLimit 10 -Parallel {
+        # generate unarchive name
+        $unarchive = Join-Path $using:OutputPath "$($_.BaseName)"
+        if (Test-Path -LiteralPath $unarchive -PathType Container) {
+            Write-Warning "Skipping: $unarchive"
             continue
         }
 
-        # generate unarchive name
-        $unarchive = Join-Path $OutputPath "$($entry.BaseName)"
-        Write-Host "Unarchiving: $unarchive"
-
         # unarchive entry
-        $command = "7z x '$entry' -mmt16 -y -o'$unarchive'"
+        Write-Host "Unarchiving: $unarchive"
+        7z x -bso0 -bsp0 "$($_.FullName)" -y -o"$unarchive"
+    }
+}
+
+# Test all archives in the given path.
+function Invoke-UnarchiveTest {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$InputPath
+    )
+
+    # list archives
+    Write-Host "Listing archived files: $InputPath"
+    $entries = Get-ChildItem $InputPath -File | Where-Object { $_.Extension -in ".7z", ".zip" }
+    Write-Host "Found $($entries.Count) entries."
+
+    # test archives
+    for ($i = 0; $i -lt $entries.Count; $i++) {
+        $entry = $entries[$i]
+        & 7z t `"$($entry.FullName)`"
+        if ($LastExitCode -ne 0) {
+            Write-Host "Failed: $($entry.FullName)"
+        }
+        Write-Progress `
+            -Activity "Testing..." `
+            -Status "$($i + 1)/$($entries.Count): $($entry.Name)" `
+            -PercentComplete ((($i + 1) / $entries.Count) * 100)
+    }
+}
+
+# Converts a zip to 7z file.
+function Invoke-ZipTo7z {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$InputPath
+    )
+
+    # get input path folder
+    $inputPath = Get-Item $InputPath
+    $inputPathFolder = ([System.IO.Path]::GetDirectoryName($inputPath))
+
+    # generate temp path
+    $inputPathBasename = [System.IO.Path]::GetFileNameWithoutExtension($inputPath)
+    $unarchived = Join-Path ([System.IO.Path]::GetTempPath()) "ZipTo7z" $inputPathBasename
+
+    # unarchive zip to temp path
+    Write-Host "`nUnarchiving: $inputPath"
+    if (Test-Path -LiteralPath $unarchived -PathType Container) {
+        Write-Warning "Skipping: $inputPath"
+    } else {
+        $command = "7z x -bso0 -bsp0 `"$inputPath`" -y -o`"$unarchived`""
         Write-Host $command
         Invoke-Expression $command
     }
+
+    # archive 7z
+    Write-Host "`nArchiving: $unarchived"
+    $archived = "$unarchived.7z"
+    if (Test-Path -LiteralPath $archived -PathType Leaf) {
+        Write-Warning "Skipping: $archived"
+    } else {
+        $command = "7z a -t7z -mx=9 -bso0 -bsp0 `"$archived`" `"$($unarchived)\*`""
+        Write-Host $command
+        Invoke-Expression $command
+    }
+
+    # move archived to same folder as input path
+    Write-Host "`nMoving: $archived to $inputPathFolder"
+    Move-Item -LiteralPath $archived -Destination $inputPathFolder -Force
 }
 
 # Hash all files in the given path and store it as AlternateDataStream.
 function Invoke-Hash {
     param (
         [Parameter(Mandatory = $true)]
-        [string[]]$Path
+        [string]$Path
     )
 
     # list files
@@ -125,7 +190,7 @@ function Invoke-Hash {
     Write-Host "Found $($files.Count) files."
 
     # hash files
-    $files | ForEach-Object -ThrottleLimit 16 -Parallel {
+    $files | ForEach-Object -ThrottleLimit 10 -Parallel {
         # get ADS hash
         $hasHash = (Get-Item -LiteralPath $_.FullName -Stream * | Where-Object { $_.Stream -eq "HASH" }).Count -gt 0
         if ($hasHash) {
@@ -156,7 +221,7 @@ function Invoke-FindDuplicates {
 
     # hash in parallel
     $processing = [System.Collections.Concurrent.ConcurrentQueue[bool]]::new()
-    $hashes = $filesToHash | ForEach-Object -ThrottleLimit 16 -Parallel {
+    $hashes = $filesToHash | ForEach-Object -ThrottleLimit 10 -Parallel {
         # increment counter and log
         $processing = $using:processing
         $processing.Enqueue($true)
@@ -193,11 +258,39 @@ function Invoke-FindDuplicates {
 # Aliases
 # ------------------------------------------------------------------------------
 
+# PowerShell aliases
+function SortSize {
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        $input
+    )
+    $input | Sort-Object Length
+}
+function SortSizeDesc {
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        $input
+    )
+    $input | Sort-Object Length -Descending
+}
+function Top {
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        $input,
+
+        [Parameter(Mandatory = $false)]
+        [int]$n = 10
+    )
+    $input | Select-Object -First $n
+}
+
 # Function aliases
 Set-Alias arc        Invoke-Archive
 Set-Alias unarc      Invoke-Unarchive
+Set-Alias unarc-test Invoke-UnarchiveTest
 Set-Alias hash       Invoke-Hash
 Set-Alias dup        Invoke-FindDuplicates
+Set-Alias zzz        Invoke-ZipTo7z
 
 # Git aliases
 function gga     { git add --all }
